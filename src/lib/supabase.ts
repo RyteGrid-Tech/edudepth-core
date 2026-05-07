c0001c0e60151f74104439d2998b34c037277125
Rofeeq Shittu <rofeeqshittu21@gmail.com>
2026-05-07 15:47:31 +0100

    chore: prepare supabase for environment variables

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://uavlqrapmcuhlhrtfvri.supabase.co";
-const SUPABASE_ANON_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVhdmxxcmFwbWN1aGxocnRmdnJpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgwMTE1MzQsImV4cCI6MjA5MzU4NzUzNH0.niba_kFTb9NS_wNBIFwGOktHwpDooHk4KmM-oc9HYQs";
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL || "https://uavlqrapmcuhlhrtfvri.supabase.co";
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVhdmxxcmFwbWN1aGxocnRmdnJpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgwMTE1MzQsImV4cCI6MjA5MzU4NzUzNH0.niba_kFTb9NS_wNBIFwGOktHwpDooHk4KmM-oc9HYQs";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
@@ -11,3 +10,4 @@ export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
     detectSessionInUrl: true,
   },
 });
+
